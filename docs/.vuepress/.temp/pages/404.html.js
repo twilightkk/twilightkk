7f02beb446ff7047b7875ba0cd9a341d16bc350a
twilightkk <29631880+twilightkk@users.noreply.github.com>
2024-06-19 09:51:45 +0000

    Deploying to gh-pages from @ twilightkk/twilightkk.github.io@afee3bb76baf4b5d5b174a62a4de9330883b3c63 🚀

--- docs/.vuepress/.temp/pages/404.html.js
+++
@@ -1,3 +0,0 @@
-import comp from "D:/git/vuepress-starter2/docs/.vuepress/.temp/pages/404.html.vue"
-const data = JSON.parse("{\"path\":\"/404.html\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{\"layout\":\"NotFound\"},\"headers\":[],\"git\":{},\"filePathRelative\":null,\"excerpt\":\"<p>404 Not Found</p>\\n\"}")
-export { comp, data }
