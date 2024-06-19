703ed8d3c134978cee5b5da96bd4eeb748eba9ce
twilightkk <twilightkk@qq.com>
2024-06-19 17:46:00 +0800

    chore:add ignore

--- docs/.vuepress/.temp/pages/category/categorya/index.html.js
+++
@@ -1,3 +0,0 @@
-import comp from "D:/git/vuepress-starter2/docs/.vuepress/.temp/pages/category/categorya/index.html.vue"
-const data = JSON.parse("{\"path\":\"/category/categorya/\",\"title\":\"Category CategoryA\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"Category CategoryA\",\"sidebar\":false,\"blog\":{\"type\":\"category\",\"name\":\"CategoryA\",\"key\":\"category\"},\"layout\":\"Category\"},\"headers\":[],\"git\":{},\"filePathRelative\":null,\"excerpt\":\"\"}")
-export { comp, data }
--- docs/.vuepress/.temp/pages/timeline/index.html.js
+++
@@ -1,3 +0,0 @@
-import comp from "D:/git/vuepress-starter2/docs/.vuepress/.temp/pages/timeline/index.html.vue"
-const data = JSON.parse("{\"path\":\"/timeline/\",\"title\":\"Timeline\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"Timeline\",\"sidebar\":false,\"blog\":{\"type\":\"type\",\"key\":\"timeline\"},\"layout\":\"Timeline\"},\"headers\":[],\"git\":{},\"filePathRelative\":null,\"excerpt\":\"\"}")
-export { comp, data }
